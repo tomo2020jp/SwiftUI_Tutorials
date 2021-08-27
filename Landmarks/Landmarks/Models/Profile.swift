31721a4c8b4cc15737e2a79dc6e7dd156e2ab007
bruno1911 <39954318+bruno1911@users.noreply.github.com>
2021-08-27 16:33:53 +0900

    add Working with UI Controls

---
+++ Landmarks/Landmarks/Models/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by tomohiko on 2021/08/27.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotification = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable{
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String{ self.rawValue }
+    }
+}
